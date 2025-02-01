b7e88291968c81c422a7064c1f926856593f3b52
Otávio Campos de Abreu Serra <otavioserra@gmail.com>
2025-02-01 15:12:39 -0300

    Initial configuration of React by installing @wordpress/create-block

---
+++ build/wordpress-assessment/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'f8715c3e2ecc0bcd5208');
---
+++ build/wordpress-assessment/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '14067a22e8ea8733f636');
