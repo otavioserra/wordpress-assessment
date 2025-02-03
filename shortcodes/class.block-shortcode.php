9a60ea69fb59e6e70ed503f18b661a9e43435fdd
Otávio Campos de Abreu Serra <otavioserra@gmail.com>
2025-02-03 19:27:57 -0300

    Block_Shortcode::add_shortcode: fixing bugs: #14

--- shortcodes/class.block-shortcode.php
+++ shortcodes/class.block-shortcode.php
@@ -7,23 +7,29 @@ if( ! class_exists( 'Block_Shortcode' ) ){
         }
 
         public function add_shortcode( $atts = array() ){
-            $atts = shortcode_atts( array(
-                // More attrs here
-            ), $atts, OS_BLOCK_SHORTCODE_ID );
-        
-            // Create the block array
-            $block = array(
-                'blockName' => OS_BLOCK_ID, // Name of your block
-                'attrs' => $atts, // Block attributes
-            );
+            $block_name = OS_BLOCK_ID; // The name of the block
+            $block_instance = null;
 
-            return 'Block String: '.print_r(WP_Block_Type_Registry::get_instance()->get_all_registered(),true).' ### '.render_block( $block );
-        
-            // Render the block
-            if ( function_exists( 'render_block' ) ) {
-                return render_block( $block );
+            // Search for all registered instances of the block. There may be more than one!
+            $block_types = WP_Block_Type_Registry::get_instance()->get_registered( $block_name );
+
+            if ( ! empty( $block_types ) ) {
+                foreach ( $block_types as $block_type ) {
+
+                    // Dynamically creates a block with the default attributes. Adjust if your block has specific attributes.
+                    $block_instance = array(
+                        'blockName' => $block_name,
+                        'attrs' => array(),
+                    );
+
+                    break; // Stop iterating after finding the first block.
+                }
+            }
+
+            if ( $block_instance ) {
+                return render_block( $block_instance );
             } else {
-                return do_blocks( $content );
+                return 'Block not found.';
             }
         }
     }
