8efa67de8ced9d9b3fcae8ff379aa5fa4a1b3c27
Otávio Campos de Abreu Serra <otavioserra@gmail.com>
2025-02-03 19:51:44 -0300

    Block_Shortcode::add_shortcode: fixing bugs: #18

--- shortcodes/class.block-shortcode.php
+++ shortcodes/class.block-shortcode.php
@@ -7,34 +7,7 @@ if( ! class_exists( 'Block_Shortcode' ) ){
         }
 
         public function add_shortcode( $atts = array() ){
-            $block_name = 'assessment/otavio-serra-plugin';
-            $block_instance = null;
-
-            $block_types = WP_Block_Type_Registry::get_instance()->get_registered( $block_name );
-
-            if (! empty( $block_types ) ) {
-                foreach ($block_types as $block_type) {
-
-                    $attributes = isset($block_type->attributes)? $block_type->attributes: array();
-
-                    $block_instance = array(
-                        'blockName' => $block_name,
-                        'attrs' => $attributes,
-                    );
-
-                    break;
-                }
-            }
-
-            if ( $block_instance ) {
-
-                $output =  $block_types[$block_name]->render_callback( $block_instance['attrs'] );
-
-                return $output;
-
-            } else {
-                return 'Bloco não encontrado.';
-            }
+            return '<p class="wp-block-assessment-otavio-serra-plugin"><section class="wa-section-container"><header class="wa-header-container"><h3 class="wa-header-title">Interview Development Position</h3><p class="wa-header-description">Fill all the form and click on submit button to send the form and start to enter in a job assessment</p></header><form class="wa-form-container"><div class="wa-grid-cols-2"><div class="wa-default-container"><input type="text" name="first_name" id="first_name" class="wa-input" placeholder=" " required/><label for="first_name" class="wa-label">First Name</label></div><div class="wa-default-container"><input type="text" name="last_name" id="last_name" class="wa-input" placeholder=" " required/><label for="last_name" class="wa-label">Last Name</label></div></div><div class="wa-grid-cols-2"><div class="wa-default-container"><input type="tel" name="phone" id="phone" class="wa-input" placeholder=" " required/><label for="phone" class="wa-label">Phone Number</label></div><div class="wa-default-container"><input type="date" name="birthdate" id="birthdate" class="wa-input" required/><label for="birthdate" class="wa-label">Birthdate</label></div></div><div class="wa-default-container"><input type="email" name="email" id="email" class="wa-input" placeholder=" " required/><label for="email" class="wa-label">Email Address</label></div><div class="wa-default-container"><input type="text" name="country" id="country" class="wa-input" placeholder=" " required/><label for="country" class="wa-label">Country</label></div><div class="wa-default-container"><textarea name="bioOrResume" id="bioOrResume" class="wa-input" placeholder=" " required></textarea><label for="country" class="wa-label">Short Bio or Resume</label></div><button type="submit" class="wa-button">Submit</button></form></section></p>';
         }
     }
 }
