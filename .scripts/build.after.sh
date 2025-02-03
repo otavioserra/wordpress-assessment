28a0097a392db9b35d3782d321b3bcad63586a01
Otávio Campos de Abreu Serra <otavioserra@gmail.com>
2025-02-03 11:41:03 -0300

    I finished the environment on VS Code with the final tweaks.

---
+++ .scripts/build.after.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Define the destination folder where you want to copy the files
+DESTINATION_DIR="/c/Users/otavi/OneDrive/Documentos/Web/wordpress/wp-content/plugins/otavio-serra-plugin"
+
+# Check if the directory exists
+if [ ! -d "$DESTINATION_DIR" ]; then
+  # Create the directory
+  echo "Creating the directory: " . "$DESTINATION_DIR";
+  mkdir -p "$DESTINATION_DIR"
+fi
+
+unset GIT_INDEX_FILE
+echo "Start copy files to: " . "$DESTINATION_DIR";
+
+# Copy files to the destination folder
+rsync -av --include='/build/' --include='/build/**/*' --exclude='/*' . "$DESTINATION_DIR"
+
+echo "End copy";
+exit 0;
--- otavio-serra-plugin.php
+++ otavio-serra-plugin.php
@@ -9,8 +9,6 @@
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       otavio-serra-plugin
- *
- * @package CreateBlock
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -25,6 +23,6 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @see https://developer.wordpress.org/reference/functions/register_block_type/
  */
 function create_block_otavio_serra_plugin_block_init() {
-	register_block_type( __DIR__ . '/build' );
+	register_block_type( __DIR__ );
 }
 add_action( 'init', 'create_block_otavio_serra_plugin_block_init' );
