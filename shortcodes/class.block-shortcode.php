18567ce7110d70ec19b609a7038ef5eb4f2ee88e
Otávio Campos de Abreu Serra <otavioserra@gmail.com>
2025-02-03 19:46:22 -0300

    Block_Shortcode::add_shortcode: fixing bugs: #18

--- shortcodes/class.block-shortcode.php
+++ shortcodes/class.block-shortcode.php
@@ -7,29 +7,33 @@ if( ! class_exists( 'Block_Shortcode' ) ){
         }
 
         public function add_shortcode( $atts = array() ){
-            $block_name = OS_BLOCK_ID; // The name of the block
+            $block_name = 'assessment/otavio-serra-plugin';
             $block_instance = null;
 
-            // Search for all registered instances of the block. There may be more than one!
             $block_types = WP_Block_Type_Registry::get_instance()->get_registered( $block_name );
 
-            if ( ! empty( $block_types ) ) {
-                foreach ( $block_types as $block_type ) {
+            if (! empty( $block_types ) ) {
+                foreach ($block_types as $block_type) {
 
-                    // Dynamically creates a block with the default attributes. Adjust if your block has specific attributes.
+                    $attributes = isset($block_type->attributes)? $block_type->attributes: array();
+
                     $block_instance = array(
                         'blockName' => $block_name,
-                        'attrs' => array(),
+                        'attrs' => $attributes,
                     );
 
-                    break; // Stop iterating after finding the first block.
+                    break;
                 }
             }
 
             if ( $block_instance ) {
-                return render_block( $block_instance );
+
+                $output =  $block_types[$block_name]->render_callback( $block_instance['attrs'] );
+
+                return $output;
+
             } else {
-                return 'Block not found.';
+                return 'Bloco não encontrado.';
             }
         }
     }
