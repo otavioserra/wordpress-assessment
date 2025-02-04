f16de8ef4799d9c12ef7474444e5203cb605a6a8
Otávio Campos de Abreu Serra <otavioserra@gmail.com>
2025-02-04 10:11:02 -0300

    Selector updated

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => '3501daff121956b543eb');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => '5db680a491fc829345b5');
